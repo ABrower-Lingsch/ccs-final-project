479792734b1bcca7632f53ce0f291d43b4494b80
Adam Brower-Lingsch <adambrower-lingsch@Adams-MacBook-Air.local>
2022-11-08 16:25:46 -0500

    updates stylist profile creation

--- frontend/static/src/components/Profile/StylistCreate.js
+++ frontend/static/src/components/Profile/StylistCreate.js
@@ -1,4 +1,14 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import Cookies from "js-cookie";
+import defaultImage from "../../Images/default2.jpg"
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
+
+const INITIAL_STYLIST_STATE = {
+    
+}
 
 function CreateStylistProfile() {
     return (
